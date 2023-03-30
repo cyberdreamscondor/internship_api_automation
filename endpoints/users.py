1e79cee532db27e1fc29a687c802a45ac621a083
HamasikQA <hamasik.mkrtchian@gmail.com>
2023-03-30 13:17:32 +0400

    final project commit

--- endpoints/users.py
+++ endpoints/users.py
@@ -2,10 +2,18 @@ from base.base_api import BaseApi
 import json
 
 get_users_endpoint = "/public/v2/users"
+get_user_endpoint = "/public/v2/users/"
+create_user_endpoint = "/public/v2/users"
+update_user_endpoint = "/public/v2/users"
+delete_user_endpoint = "/public/v2/users/"
 
 
 class Users(BaseApi):
-    def get_users(self, url, expected_status_code):
+    def get_users(self, url, expected_status_code, user_id):
         response = self.get_request(url + get_users_endpoint)
+        self.check_status_code(response, expected_status_code)
+
+    def get_user_by_id(self, url, expected_status_code, id):
+        response = self.get_request(url + get_user_endpoint + id)
         self.check_status_code(response, expected_status_code)
 
--- tests/tests_users.py
+++ tests/tests_users.py
@@ -4,3 +4,7 @@ from endpoints.users import Users
 def test_get_users(app_config):
     users = Users()
     users.get_users(app_config.base_url, 200)
+
+def test_get_user(app_config):
+    users = Users()
+    users.get_user_by_id(app_config.base_url, 200, "292")
