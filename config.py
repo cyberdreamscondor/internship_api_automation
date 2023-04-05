cee57f071794fb364dab23a02358e85703d71d0d
David Akopyan <cyberdreamscondor@hotmail.com>
2023-04-05 17:25:39 +0400

    user endpoint functions

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
             'local': ":80"
         }[env]
 
-        self.token ={
+        self.token = {
             "Authorization": "Bearer c8816a6b5a1ce16041be702d5b0640ed4ba524d18bd27eb35b0f09eed255845f"
         }
 
--- endpoints/users.py
+++ endpoints/users.py
@@ -4,24 +4,30 @@ from base.base_api import BaseApi
 class Users(BaseApi):
     endpoint = "/public/v2/users/"
 
-    def get_users(self, url, expected_status_code):
-        response = self.get_request(url+self.endpoint)
+    def get_users(self, url,  expected_status_code, headers=None):
+        response = self.get_request(url+self.endpoint, headers=headers)
         self.check_status_code(response, expected_status_code)
+        return response
 
-    def get_user(self, url, user_id, expected_status_code):
-        response = self.get_request(url+self.endpoint+str(user_id), expected_status_code)
+    def get_user(self, url, user_id, expected_status_code, headers=None):
+        response = self.get_request(url+self.endpoint+str(user_id), headers=headers)
         self.check_status_code(response, expected_status_code)
+        return response
 
-    def create_user(self):
-        pass
+    def create_user(self, url, data, expected_status_code, headers=None):
+        response = self.post_request(url+self.endpoint, data, headers=headers)
+        self.check_status_code(response, expected_status_code)
+        return response
 
-    def update_user(self):
-        pass
+    def update_user(self, url, data, expected_status_code, headers=None):
+        response = self.put_request(url+self.endpoint+str(id), data, headers=headers)
+        self.check_status_code(response, expected_status_code)
+        return response
 
-    def delete_user(self):
-        pass
-
-
+    def delete_user(self, url, expected_status_code, headers=None):
+        response = self.delete_request(url+self.endpoint+str(id), headers=headers)
+        self.check_status_code(response, expected_status_code)
+        return response
 
 
 
