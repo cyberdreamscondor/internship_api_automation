04f0746f74455b09e18532525fb383dfb71a33a3
David Akopyan <cyberdreamscondor@hotmail.com>
2023-04-14 05:42:16 +0400

    create xml user

--- tests/tests_users.py
+++ tests/tests_users.py
@@ -3,6 +3,7 @@ import json
 from endpoints.users import Users
 import utils.jsonmodels.users_json as mock_data
 import jsonschema
+import xml.etree.ElementTree as ET
 
 
 class UsersTests(Users):
@@ -80,12 +81,37 @@ class UsersTests(Users):
         }
 
         # There's no such message, let us suppose it has to be
+        # actually what is the best way? Mark as failing, raise error?
+
         expected_response = [{
             "field": "name",
             "message": "can't be a number"
         }]
         response = self.create_user(app_config.base_url, invalid_data, 422, app_config.token)
         assert expected_response == response.json()
+
+
+    def test_xml_create_user(self, app_config):
+        """
+        Tests whether xml data is accepted
+        """
+        headers = {
+            **app_config.token,
+            "Content-Type": "application/xml"
+        }
+
+        user_data = mock_data.create_user_xml_data("Dave", "emailmeplease@fortest.com", "male", "active")
+        response = self.post_request('https://gorest.co.in/public/v2/users.xml', user_data, headers=headers)
+        root = ET.fromstring(response.content)
+        #ET.dump(root) prints just like you wanna see it
+
+        assert response.status_code == 201
+        assert response.headers["Content-Type"] == "application/xml"
+        assert root.find("data/name").text == "Dave"
+
+
+
+
 
 
 
